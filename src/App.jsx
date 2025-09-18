bfdca3c722be8eb4ac741c30e72b2fcf187f9251
aklademarco <aklademarco2@gmail.com>
2025-09-18 21:58:35 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-amber-300'>Vite + React</h1>
+      <h1 className='text-teal-900'>helloo
+        <span className='text-rose-500'> world</span>
+      </h1>
+    </>
+  )
+}
+
+export default App
+
+ 
